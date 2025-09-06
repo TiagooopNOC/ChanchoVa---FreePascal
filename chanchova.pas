0a04c0bfa250a619df626e106cabc414ecf7982b
tiagoopnoc <tiagonocella@gmail.com>
2025-09-06 15:51:41 -0300

    Added Barajar, Repartir and created const string to name the Palos easily.

--- chanchova.pas
+++ chanchova.pas
@@ -9,21 +9,21 @@ numero:integer;
 palo:palos; 
 end;
 
+TMano = array[1..4] of TCarta;
+
 TMazo = array [1..16] of TCarta;
 
 var
 i:integer;
 p:integer;
+
+CartasP1,CartasP2,CartasP3,CartasP4: TMano;
+
 MazoJuego:TMazo;
 
-CartasP1: array [1..4] of TCarta;
-
-CartasP2: array [1..4] of TCarta;
-
-CartasP3: array [1..4] of TCarta;
-
-CartasP4: array [1..4] of TCarta;
-
+const NombrePalos: array[palos] of string = ('Espada', 'Oro', 'Copa', 'Basto');
+{Toco usar string para nombrar los palos porque un viaje con char.}
+//------------------------------------------------------------------------------------------------------
 
 procedure LlenarMazo(var Mazo:Tmazo); 
 //Inicializar el Mazo con sus cartas para 4 jugadores. (1 de espada, 1 de copa .. 2 de espada ... , 3 de espada, ... 4 de espada, ...)
@@ -42,39 +42,63 @@ begin
     end;
 end;
 
-procedure Barajar(Mazo:TMazo);
+procedure Barajar(var Mazo:TMazo);
+{Implementado lógica de shuffle (barajado) de Fisher-Yates}
 var
 i,j:integer;
 temp:TCarta;
 begin
   randomize;
-  for i:= 1 to 16 do
+  for i:= 16 downto 2 do
   begin
-    Mazo[i]
+    j:= random (i) + 1;
+    temp:=Mazo[i];
+    Mazo[i] := Mazo[j];
+    Mazo[j] := temp;
   end;
 end;
 
-{function CartaEnUso(CartasP:TCarta):boolean;
-var i:integer;
-}
-
-{procedure RepartirCartas(var CartasP:TCarta; var Mazo);
-var i,j:integer;
+procedure Repartir(var Mazo:TMazo; var p1,p2,p3,p4:TMano);
+{Aplicado Lógica de Repartir ( Simulando de arriba a abajo )}
+var i,indice:integer;
 begin
-  for i:=1 to  4 do
+  indice:=1;
+  for i:=1 to 4 do
     begin
-      randomize;
-      CartasP[i].numero:= (random(4) + 1);
-
+      p1[i]:=Mazo[indice];
+      indice:=indice+1;
+      p2[i]:=Mazo[indice];
+      indice:=indice+1;
+      p3[i]:=Mazo[indice];
+      indice:=indice+1;
+      p4[i]:=Mazo[indice];
+      indice:=indice+1;
     end;
-end;}
+end;
 
 begin
-    randomize;
     LlenarMazo(MazoJuego);
-    for i:= 1 to 4 do
-        for p:= 1 to 4 do
-        begin
-        writeln(MazoJuego[i].numero,' ', MazoJuego[p].palo);
-        end;
+    Barajar(MazoJuego);
+
+    for i:= 1 to 16 do
+    writeln(MazoJuego[i].numero, ' De ', NombrePalos[MazoJuego[i].palo]);
+    writeln;
+    writeln('----------------------------------------------------------------');
+    writeln;
+
+    Repartir(MazoJuego,CartasP1,CartasP2,CartasP3,CartasP4);
+    for i:=1 to 4 do
+    {Escribimos que carta le toca a cada jugador}
+    begin
+      writeln('Cartas ',i  ,' P1');
+      writeln(CartasP1[i].numero, ' De ', NombrePalos[CartasP1[i].palo]);
+      writeln('Cartas ',i  ,' P2');
+      writeln(CartasP2[i].numero, ' De ', NombrePalos[CartasP2[i].palo]);
+      writeln('Cartas ',i  ,' P3');
+      writeln(CartasP3[i].numero, ' De ', NombrePalos[CartasP3[i].palo]);
+      writeln('Cartas ',i  ,' P4');
+      writeln(CartasP4[i].numero, ' De ', NombrePalos[CartasP4[i].palo]);
+    end;
+
+  {Falta aplicar movimiento de juego del chancho va e interactividad con whiles y centinelas.}
 end.
